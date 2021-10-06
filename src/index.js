0dbb4e8bd62d29e237a94ef42efb01f23197093b
alexdeia <alexk.deia@gmail.com>
2021-10-06 22:44:07 +0300

    feat: add localforage as offline storage, fix bug with options

---
+++ src/index.js
@@ -0,0 +1,94 @@
+import localforage from 'localforage';
+
+/* ----------------------- Mixin ------------------------ */
+
+/** This mixin adds:
+ * - `isOnline`, `isOffline` data properties
+ * - `online`, `offline` in-component events
+ * */
+export const VueOfflineMixin = {
+  data() {
+    return {
+      isOnline: false,
+      isOffline: false,
+    };
+  },
+  mounted() {
+    if (typeof window !== 'undefined') {
+      navigator.onLine ? (this.isOnline = true) : (this.isOffline = true);
+
+      const onlineHandler = () => {
+        this.$emit('online');
+        this.isOnline = true;
+        this.isOffline = false;
+      };
+
+      const offlineHandler = () => {
+        this.$emit('offline');
+        this.isOffline = true;
+        this.isOnline = false;
+      };
+
+      window.addEventListener('online', onlineHandler);
+      window.addEventListener('offline', offlineHandler);
+
+      this.$once('hook:beforeDestroy', () => {
+        window.removeEventListener('online', onlineHandler);
+        window.removeEventListener('offline', offlineHandler);
+      });
+    }
+  },
+};
+
+/* ----------------------- Storage ------------------------ */
+
+function _addKey(newKey) {
+  let keys = JSON.parse(localStorage.getItem('VueOfflineStorageKeys')) || [];
+  if (!keys.includes(newKey)) keys.push(newKey);
+  localStorage.setItem('VueOfflineStorageKeys', JSON.stringify(keys));
+}
+
+export const VueOfflineStorage = {
+  init(options = {}) {
+    const localforageInstances = localforage.createInstance(options);
+
+    if (!options.instances) {
+      return localforageInstances;
+    }
+
+    for (const instance of options.instances) {
+      const name = instance.storeName || instance.name;
+
+      if (!name) {
+        continue;
+      }
+
+      localforageInstances[name] = localforage.createInstance(instance);
+    }
+  },
+};
+
+/* ----------------------- Plugin ------------------------ */
+
+/** Registers VueOfflineMixin in whole application giving you access to:
+ * - isOnline, isOffline data properties
+ * - online, offline in-component events
+ */
+export const VueOfflinePlugin = {
+  install(Vue, options = {}) {
+    const defaultOptions = {
+      mixin: true,
+      storage: {},
+    };
+
+    const pluginOptions = {
+      ...defaultOptions,
+      ...options,
+    };
+    if (pluginOptions.storage)
+      Vue.prototype.$offlineStorage = VueOfflineStorage.init(options.storage);
+    if (pluginOptions.mixin) Vue.mixin(VueOfflineMixin);
+  },
+};
+
+export default VueOfflinePlugin;
